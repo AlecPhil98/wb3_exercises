8205ae39bbd199b4f42ea99965df41a65f9e9c69
Alec Philavong <alecphil@live.com>
2024-04-30 13:05:05 -0500

    completed simple_functions.js

---
+++ funtions/simple_funtions.js
@@ -0,0 +1,28 @@
+"use strict"
+//function to display my name, favorite movie, and favorite band
+function favoritethings() {
+
+    console.log("Alec")
+    console.log("Meet the Robisons")
+    console.log("Keshi")
+
+
+}
+
+
+function whyImHere() {
+    console.log("I joined this Learn to Code program to try somthing different in a new work field and to have a better work life balance rather than grueling hard labor");
+}
+
+
+
+function favoritePlace(){
+console.log("My favorite place that ive visted was Hawaii")
+
+
+}
+
+//calling/invoking our fuctions
+favoritethings();
+whyImHere();
+favoritePlace();
